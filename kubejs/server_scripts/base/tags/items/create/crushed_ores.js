f1e3e6fa1ff028cb2781ef0394590b50c9b58386
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-09-05 20:31:00 +0200

    Update crushed_ores.js
    It just looks better ^_~

--- kubejs/server_scripts/base/tags/items/create/crushed_ores.js
+++ kubejs/server_scripts/base/tags/items/create/crushed_ores.js
@@ -1,8 +1,8 @@
 onEvent('item.tags', (event) => {
-let crushed_ores = event.get('create:crushed_ores').getObjectIds()
+    let crushed_ores = event.get('create:crushed_ores').getObjectIds()
 
-crushed_ores.forEach((material) => {
-    let material_split = String(material).split('create:crushed_')[1]
-    event.add(`create:crushed_ores/${material_split}`, material)
-})
+    crushed_ores.forEach((material) => {
+        let material_split = String(material).split('create:crushed_')[1]
+        event.add(`create:crushed_ores/${material_split}`, material)
+    })
 });
